a5db7028bbf7ba08dda80129da1239fb11b595ad
Hanbyeol Lee <a36688796@gmail.com>
2024-05-16 15:47:01 +0900

    Update news_crawler()

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup       # 웹에서 가져온 HTML코드를 파이썬에서 편하게 분석해주는 라이브러리
 import re
 from datetime import datetime, timedelta
+
 """
 def google_news_crawler(query):
     # 구글 뉴스 검색 URL
@@ -57,26 +58,31 @@ if __name__ == "__main__":
 
 def news_crawler():
     # 구글 뉴스 검색 URL
-    url = "https://www.gamevu.co.kr/news/articleView.html?idxno=32810"      # URL 내에 query 변수명을 그대로 입력했더니 'query'라는 문자열을 검색한 결과가 나옴. 전에는 멀쩡했는데 왜 이러는지 모르겠음
+    url = "https://www.gamevu.co.kr/news/articleView.html?idxno=32810"
 
     # HTTP 요청 보내기
     response = requests.get(url)
+    d_response = response.content.decode('cp949','replace')
+    # 한글 폰트 깨짐. html 인코딩이 enc-kr로 되어있으면 크롤링할 때 글씨가 깨질 수 있음
+    # 그래서 utf-8이나 cp949로 인코딩을 바꿔야 함. utf-8은 그대로 깨져서 나오길래 cp949로 인코딩했더니 성공함
 
     # HTTP 요청 성공하면 HTML 파싱
     if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-
-        # 기사 정보 추출
-        press = soup.select_one('div.vr1PYe').text
-
+        soup = BeautifulSoup(d_response, 'html.parser')
+        
         # 제목 추출
-        title = soup.select_one('a.JtKRv')     # 클래스가 'JtKRv'인 a태그를 선택하고 내용 반환
+        news_title = soup.select_one('font.headline-title')
+
+        # 기사 본문 추출
+        news_d = soup.select('div.cont-body')
+        # 본문의 텍스트만 출력하고 싶은데 html의 태그까지 출력됨. 해결방안을 찾아야 할 듯
 
         # 날짜 추출
-        datetime_str = soup.select_one('div.UOVeFe time.hvbAAd')['datetime']
+        datetime_str = soup.select_one('li.date').get_text()
+        datetime_str = datetime_str[3:]
 
         # datetime 값을 파싱
-        dt = datetime.strptime(datetime_str, '%Y-%m-%dT%H:%M:%SZ')
+        dt = datetime.strptime(datetime_str, '%Y.%m.%d %H:%M')
 
         # 9시간을 더해 한국 시간으로 변환
         korean_time = dt + timedelta(hours = 9)
@@ -84,13 +90,16 @@ def news_crawler():
         # 분까지만 출력 (strftime으로 포맷)
         formatted_time = korean_time.strftime('%Y-%m-%d %H:%M')
 
-        print(press)
-        print(title)
+        print(news_title)
         print(formatted_time)
+        print(news_d)
         print("")
+        
 
     else:
         print("HTTP 요청 실패")
 
 if __name__ == "__main__":
     news_crawler()
+
+

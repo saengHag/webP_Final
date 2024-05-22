a28c82535edbbaf837eebc7674231906a036b555
Hanbyeol Lee <a36688796@gmail.com>
2024-05-22 15:44:06 +0900

    Start using fastapi

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
+from typing import Union
+from fastapi import FastAPI
 import requests
-from bs4 import BeautifulSoup       # 웹에서 가져온 HTML코드를 파이썬에서 편하게 분석해주는 라이브러리
 import re
 from datetime import datetime, timedelta
-from jinja2 import Environment, FileSystemLoader    # 진자2 학습용. 어떤 식으로 사용하는지 확인하고 주석처리할 예정
 from fastapi.templating import Jinja2Templates
+from bs4 import BeautifulSoup       # 웹에서 가져온 HTML코드를 파이썬에서 편하게 분석해주는 라이브러리
+
 
 """
 def google_news_crawler(query):
@@ -59,7 +61,7 @@ if __name__ == "__main__":
 """
 """
 def news_crawler():
-    # 구글 뉴스 검색 URL
+    # 구글 뉴스 검색으로 얻은 URL로 기사 내용을 스크랩
     url = "https://www.gamevu.co.kr/news/articleView.html?idxno=32810"
 
     # HTTP 요청 보내기
@@ -118,7 +120,7 @@ if __name__ == "__main__":
     news_crawler()
 """
 
-
+"""
 # 템플릿 파일이 있는 디렉토리 설정
 file_loader = FileSystemLoader('C:/Users/dlgkr/webProgramming/webP_Final/htmls')
 env = Environment(loader=file_loader)
@@ -138,3 +140,66 @@ output = template.render(data)
 
 # 렌더링된 템플릿 출력
 print(output)
+"""
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message":"Hello World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/search/{keyword}")
+def find_key(keyword):
+    # 구글 뉴스 검색 URL
+    url = "https://news.google.com/search?q={}&hl=ko&gl=KR&ceid=KR%3Ako".format(keyword)      # URL 내에 query 변수명을 그대로 입력했더니 'query'라는 문자열을 검색한 결과가 나옴. 전에는 멀쩡했는데 왜 이러는지 모르겠음
+
+    # HTTP 요청 보내기
+    response = requests.get(url)
+
+    # HTTP 요청 성공하면 HTML 파싱
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        # 뉴스 제목과 링크 가져오기
+        news = soup.select('c-wiz.XBspb')
+
+    
+        # 언론사 추출
+        press = news.select_one('div.vr1PYe').text
+
+        # 제목 추출
+        title = news.select_one('a.JtKRv')     # 클래스가 'JtKRv'인 a태그를 선택하고 내용 반환
+
+        # 날짜 추출
+        datetime_str = news.select_one('div.UOVeFe time.hvbAAd')['datetime']
+
+        # datetime 값을 파싱
+        dt = datetime.strptime(datetime_str, '%Y-%m-%dT%H:%M:%SZ')
+
+        # 9시간을 더해 한국 시간으로 변환
+        korean_time = dt + timedelta(hours = 9)
+
+        # 분까지만 출력 (strftime으로 포맷)
+        formatted_time = korean_time.strftime('%Y-%m-%d %H:%M')
+
+        # 링크 추출
+        link = news.select('a.JtKRv')['href']
+        link = link.lstrip('.')
+
+        print(press)
+        print(title)
+        print(formatted_time)
+        print("https://news.google.com"+link)
+        print("")
+
+    else:
+        print("HTTP 요청 실패")
+    if __name__ == "__main__":
+        return {"언론사": press, "제목": title, "작성일자": formatted_time, "링크": "https://news.google.com"+link}
+    
+    else:
+        return {"httpRequestError"}
